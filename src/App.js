6bf205c1b5ef55ce6abb2a0d2c00c5f5e96575fc
MarcoChanwoo <cwhong8087@gmail.com>
2023-08-22 16:48:20 +0900

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import ColorBox from './components/ColorBox';
+import SelectColors from './components/SelectColors';
+import { ColorProvider } from './contexts/color';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <ColorProvider>
+            <div>
+                <SelectColors />
+                <ColorBox />
+            </div>
+        </ColorProvider>
+    );
+};
 
 export default App;
